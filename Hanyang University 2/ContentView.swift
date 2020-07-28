d686a208398dc19f2ab56a58bf439eafe89afab7
Khang Nguyen <Nowondeve@gmail.com>
2020-07-28 20:07:56 +0200

    Initial Commit

---
+++ Hanyang University 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hanyang University 2
+//
+//  Created by Nowon Nguyen on 28/07/2020.
+//  Copyright © 2020 Nowon Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
